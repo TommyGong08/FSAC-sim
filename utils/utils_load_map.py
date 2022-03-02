0e4525b7eeef27bb3f7fbf3c4a97b79b99f1f63a
TommgGong08 <1034231958@qq.com>
2022-03-02 19:20:42 +0800

    add track and car model

---
+++ utils/utils_load_map.py
@@ -0,0 +1,29 @@
+import numpy
+import numpy as np
+
+
+def load_map_txt(map_file_name):
+    """
+    :param map_file_name:
+    :return: 返回列表x
+    """
+    cone_r_list = []
+    cone_b_list = []
+    with open(map_file_name, 'r') as map_file:
+        map = map_file.readlines()
+        for cone in map:
+            xx, yy, color = cone.split(' ')[0], cone.split(' ')[1], cone.split(' ')[2].split('\n')[0]
+            # print(xx, yy, color)
+            if color == "1":
+                cone_r_list.append([float(xx), float(yy)])
+            else:
+                cone_b_list.append([float(xx), float(yy)])
+    cones_r_np = np.array(cone_r_list)
+    cones_b_np = np.array(cone_b_list)
+    # print(cones_np)
+    print(cones_r_np)
+    return cones_r_np, cones_b_np
+
+
+if __name__ == '__main__':
+    load_map_txt("../map.txt")
